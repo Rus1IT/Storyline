767205e970c4fc23e847d59ea84b7cef766ec841
Rus1IT <122291247+Rus1IT@users.noreply.github.com>
2025-01-05 16:32:14 +0500

    messageServiceStoryline created

---
+++ src/main/java/root/messageservicestoryline/email/EmailService.java
@@ -0,0 +1,29 @@
+package root.messageservicestoryline.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import root.messageservicestoryline.dto.EmailDTO;
+
+@Service
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(EmailDTO emailDTO) {
+        if(emailDTO == null){
+            throw new NullPointerException("emailDTO is null");
+        }
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(emailDTO.to());
+        message.setSubject(emailDTO.subject());
+        message.setText(emailDTO.body());
+        mailSender.send(message);
+    }
+}
---
+++ src/test/java/root/messageservicestoryline/MessageServiceStorylineApplicationTests.java
@@ -0,0 +1,13 @@
+package root.messageservicestoryline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceStorylineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
